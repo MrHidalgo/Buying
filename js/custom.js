30bfe2ff3790586e30a912e6b254bcc48a3c3325
MrHidalgo <Luckyhidalgo@gmail.com>
2018-07-05 22:48:57 +0300

    Create footer - w\ adaptive.

--- js/custom.js
+++ js/custom.js
@@ -402,7 +402,7 @@ function initializeClock(id, endtime) {
   var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+  // var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
@@ -410,7 +410,7 @@ function initializeClock(id, endtime) {
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    // secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
